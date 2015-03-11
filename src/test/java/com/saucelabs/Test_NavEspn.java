a88c83687c202bf0640eedfcf97421905b709fc1
rduvalwa5 <rduvalwa5@hotmail.com>
2015-03-11 11:32:31 -0700

    Change test name

--- src/test/java/com/saucelabs/Test_NavEspn.java
+++ src/test/java/com/saucelabs/Test_NavEspn.java
@@ -114,7 +114,7 @@ public class Test_NavEspn implements SauceOnDemandSessionIdProvider {
 	            capabilities.setCapability(CapabilityType.VERSION, version);
 	        }
 	        capabilities.setCapability(CapabilityType.PLATFORM, os);
-	        capabilities.setCapability("name", "Sauce Sample Test");
+	        capabilities.setCapability("name", "Sauce ESPN Test");
 	        this.driver = new RemoteWebDriver(
 	                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
 	                capabilities);
